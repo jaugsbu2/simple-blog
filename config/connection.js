437a532a8cef08e9b09ed4d3e5ca6f05d323c4ea
jaugsbu2 <115282283+jaugsbu2@users.noreply.github.com>
2023-02-13 19:11:18 -0700

    expose heroku db info

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,12 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  "heroku_6ee795b7b3f8f84",
+  "b4d3060a6be01c",
+  "8a36b9f2",
+  // process.env.DB_NAME,
+  // process.env.DB_USER,
+  // process.env.DB_PASSWORD,
   {
     host: "us-cdbr-east-06.cleardb.net",
     dialect: 'mysql',
