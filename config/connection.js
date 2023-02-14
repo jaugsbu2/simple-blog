0c2705d526ed67c975739e9a867fe9fd6920122f
jaugsbu2 <115282283+jaugsbu2@users.noreply.github.com>
2023-02-13 19:08:36 -0700

    put db info in .env file

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,9 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  "heroku_6ee795b7b3f8f84",
-  "b4d3060a6be01c",
-  "8a36b9f2",
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
     host: "us-cdbr-east-06.cleardb.net",
     dialect: 'mysql',
